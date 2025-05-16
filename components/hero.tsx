031d8075aeb9539b00c7614c31ddce59161cdcac
yuki <yuki.for.neutral@gmail.com>
2025-05-16 12:57:58 +0900

    🎨 Update styles for various components

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <h1 className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold">
+      <h1 className="text-3xl lg:text-4xl leading-tight! mx-auto max-w-xl text-center font-bold">
         Supabase + Next.js Chat App
       </h1>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+      <div className="w-full p-px bg-linear-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
 }
